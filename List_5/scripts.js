6212f8e0f73cf6f84798e28664001cf40a489bec
wdomini4801 <91901855+wdomini4801@users.noreply.github.com>
2022-12-11 21:42:56 +0100

    done document.writeln()

---
+++ List_5/scripts.js
@@ -0,0 +1,20 @@
+function writeHeader() {
+    var location = window.location.pathname;
+    var log;
+
+    if (location.endsWith("index.html")) {
+        log = "This site contains the most important information about Islam.";
+    }
+    else if (location.endsWith("history.html")) {
+        log = "A brief history of Islam.";
+    }
+    else if (location.endsWith("pillars.html")) {
+        log = "Description of The Five Pillars of Islam.";
+    }
+    else if (location.endsWith("society.html")) {
+        log = "Information about society of Islam.";
+    }
+    document.writeln("<h2>" + log + "</h2>");
+}
+
+window.onload = writeHeader();
